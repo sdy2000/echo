45e5250a212e010e2d3d2a958339ae35f4026a8b
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2025-11-02 22:24:24 +0330

    02: convex package

---
+++ packages/backend/convex/users.ts
@@ -0,0 +1,19 @@
+import { mutation, query } from "./_generated/server";
+
+export const getMany = query({
+  args: {},
+  handler: async (ctx) => {
+    const users = await ctx.db.query("users").collect();
+
+    return users;
+  },
+});
+
+export const ad = mutation({
+  args: {},
+  handler: async (ctx) => {
+    const userId = await ctx.db.insert("users", { name: "sara" });
+
+    return userId;
+  },
+});
