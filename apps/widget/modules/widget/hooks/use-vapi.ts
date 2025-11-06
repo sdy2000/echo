2cfaaad2f44cad674d310ce441594cd9c805f931
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2025-11-06 17:34:30 +0330

    06: AI voice assistant

---
+++ apps/widget/modules/widget/hooks/use-vapi.ts
@@ -0,0 +1,86 @@
+import Vapi from "@vapi-ai/web";
+import { useEffect, useState } from "react";
+
+interface TranscriptMessage {
+  role: "user" | "assistant";
+  text: string;
+}
+
+export const useVapi = () => {
+  const [vapi, setVapi] = useState<Vapi | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [isConnecting, setIsConnecting] = useState(false);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [transcript, setTranscript] = useState<TranscriptMessage[]>([]);
+
+  useEffect(() => {
+    // Only for testing the Vapi API, otherwise customers will provide their own API keys.
+    const vapiInstance = new Vapi("2bb090e7-3f05-4969-88d4-2eae20a07650");
+
+    setVapi(vapiInstance);
+
+    vapiInstance.on("call-start", () => {
+      setIsConnected(true);
+      setIsConnecting(false);
+      setTranscript([]);
+    });
+
+    vapiInstance.on("call-end", () => {
+      setIsConnected(false);
+      setIsConnecting(false);
+      setIsSpeaking(false);
+    });
+
+    vapiInstance.on("speech-start", () => {
+      setIsSpeaking(true);
+    });
+
+    vapiInstance.on("speech-end", () => {
+      setIsSpeaking(false);
+    });
+
+    vapiInstance.on("error", (error) => {
+      console.log(error, "VAPI_ERROR");
+      setIsConnected(false);
+    });
+
+    vapiInstance.on("message", (message) => {
+      if (message.type === "transcript" && message.transcriptType === "final")
+        setTranscript((prev) => [
+          ...prev,
+          {
+            role: message.role === "user" ? "user" : "assistant",
+            text: message.transcript,
+          },
+        ]);
+    });
+
+    return () => {
+      vapiInstance?.stop();
+    };
+  }, []);
+
+  const starCall = () => {
+    setIsConnected(true);
+
+    if (vapi) {
+      // Only for testing the Vapi API, otherwise customers will provide their own Assistant IDs.
+      vapi.start("1f88c1cd-60e6-4143-a4f4-48efab3080d5");
+    }
+  };
+
+  const endCall = () => {
+    if (vapi) {
+      vapi.stop();
+    }
+  };
+
+  return {
+    isSpeaking,
+    isConnecting,
+    isConnected,
+    transcript,
+    starCall,
+    endCall,
+  };
+};
