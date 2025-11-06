bf5582f84dcafe4dbf81b8f667750c76dea2a9fd
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2025-11-06 14:08:00 +0330

    05: error tracking

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <p>apps/web</p>
       <UserButton />
       <OrganizationSwitcher hidePersonal />
+      <Button onClick={() => addUser()}>Add</Button>
       <div>{JSON.stringify(users)}</div>
     </div>
   );
